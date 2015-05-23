1bfc2525234bbeb8b32f057f2647b2ba27756a1c
Kai Szuttor <kai@icp.uni-stuttgart.de>
2015-05-23 10:21:06 +0200

    autopep8'd the code

--- kaipy/observable.py
+++ kaipy/observable.py
@@ -1,41 +1,42 @@
 # This file is part of kaipy.
 # Copyright (C) 2015  Kai Szuttor
-# 
+#
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
 # the Free Software Foundation, either version 3 of the License, or
 # (at your option) any later version.
-# 
+#
 # This program is distributed in the hope that it will be useful,
 # but WITHOUT ANY WARRANTY; without even the implied warranty of
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 # GNU General Public License for more details.
-# 
+#
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 import numpy as np
 
+
 def second_legendre(pos1, pos2, direction):
-  '''
-  Calculates second legendre polonomial of two given position vectors
-  for the angle of (pos2-pos1, direction). direction can either be:
-    - x (1,0,0)
-    - y (0,1,0)
-    - z (0,0,1)
-  '''
-  vec1 = pos1 - pos2
-  try:
-    if (direction == 'x'):
-      vec2 = np.array([1,0,0])
-    elif (direction == 'y'):
-      vec2 = np.array([0,1,0])
-    elif (direction == 'z'):
-      vec2 = np.array([0,0,1])
-  except:
-    raise ValueError("Argument must be 'x','y' or 'z'")
-  v1_norm = np.sqrt((vec1*vec1).sum())
-  v2_norm = np.sqrt((vec2*vec2).sum())
-  cos_angle = np.dot(vec1,vec2)/(v1_norm*v2_norm)
-  second_legendre = 0.5*(3*cos_angle*cos_angle-1)
-  return second_legendre
+    """
+    Calculates second legendre polonomial of two given position vectors
+    for the angle of (pos2-pos1, direction). direction can either be:
+      - x (1,0,0)
+      - y (0,1,0)
+      - z (0,0,1)
+    """
+    vec1 = pos1 - pos2
+    try:
+        if (direction == 'x'):
+            vec2 = np.array([1, 0, 0])
+        elif (direction == 'y'):
+            vec2 = np.array([0, 1, 0])
+        elif (direction == 'z'):
+            vec2 = np.array([0, 0, 1])
+    except:
+        raise ValueError("Argument must be 'x','y' or 'z'")
+    v1_norm = np.sqrt((vec1 * vec1).sum())
+    v2_norm = np.sqrt((vec2 * vec2).sum())
+    cos_angle = np.dot(vec1, vec2) / (v1_norm * v2_norm)
+    second_legendre = 0.5 * (3 * cos_angle * cos_angle - 1)
+    return second_legendre
--- kaipy/statistic.py
+++ kaipy/statistic.py
@@ -1,16 +1,16 @@
 # This file is part of kaipy.
 # Copyright (C) 2015  Kai Szuttor
-# 
+#
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
 # the Free Software Foundation, either version 3 of the License, or
 # (at your option) any later version.
-# 
+#
 # This program is distributed in the hope that it will be useful,
 # but WITHOUT ANY WARRANTY; without even the implied warranty of
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 # GNU General Public License for more details.
-# 
+#
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
@@ -46,98 +46,101 @@
 
 import numpy as np
 
+
 def _bit_length_26(x):
-  if x == 0:
-    return 0
-  elif x == 1:
-    return 1
-  else:
-    return len(bin(x)) - 2
+    if x == 0:
+        return 0
+    elif x == 1:
+        return 1
+    else:
+        return len(bin(x)) - 2
+
 
 def _next_regular(target):
-  """
-  Find the next regular number greater than or equal to target.
-  Regular numbers are composites of the prime factors 2, 3, and 5.
-  Also known as 5-smooth numbers or Hamming numbers, these are the optimal
-  size for inputs to FFTPACK.
+    """
+    Find the next regular number greater than or equal to target.
+    Regular numbers are composites of the prime factors 2, 3, and 5.
+    Also known as 5-smooth numbers or Hamming numbers, these are the optimal
+    size for inputs to FFTPACK.
 
-  Target must be a positive integer.
-  """
-  if target <= 6:
-    return target
+    Target must be a positive integer.
+    """
+    if target <= 6:
+        return target
 
-  # Quickly check if it's already a power of 2
-  if not (target & (target - 1)):
-    return target
+    # Quickly check if it's already a power of 2
+    if not (target & (target - 1)):
+        return target
 
-  match = float('inf')  # Anything found will be smaller
-  p5 = 1
-  while p5 < target:
-    p35 = p5
-    while p35 < target:
-      # Ceiling integer division, avoiding conversion to float
-      # (quotient = ceil(target / p35))
-      quotient = -(-target // p35)
-      # Quickly find next power of 2 >= quotient
-      try:
-        p2 = 2 ** ((quotient - 1).bit_length())
-      except AttributeError:
-        # Fallback for Python <2.7
-        p2 = 2 ** _bit_length_26(quotient - 1)
+    match = float('inf')  # Anything found will be smaller
+    p5 = 1
+    while p5 < target:
+        p35 = p5
+        while p35 < target:
+            # Ceiling integer division, avoiding conversion to float
+            # (quotient = ceil(target / p35))
+            quotient = -(-target // p35)
+            # Quickly find next power of 2 >= quotient
+            try:
+                p2 = 2 ** ((quotient - 1).bit_length())
+            except AttributeError:
+                # Fallback for Python <2.7
+                p2 = 2 ** _bit_length_26(quotient - 1)
 
-      N = p2 * p35
-      if N == target:
-        return N
-      elif N < match:
-        match = N
-      p35 *= 3
-      if p35 == target:
-        return p35
-    if p35 < match:
-      match = p35
-    p5 *= 5
-    if p5 == target:
-      return p5
-  if p5 < match:
-    match = p5
-  return match
+            N = p2 * p35
+            if N == target:
+                return N
+            elif N < match:
+                match = N
+            p35 *= 3
+            if p35 == target:
+                return p35
+        if p35 < match:
+            match = p35
+        p5 *= 5
+        if p5 == target:
+            return p5
+    if p5 < match:
+        match = p5
+    return match
+
 
 def autocorrelation(x, nlags=50):
-  """
-  Compute autocorrelation using FFT
-  """
-  nobs = len(x)
-  x = np.squeeze(np.asarray(x))
-  x0 = x - x.mean()
-  n = _next_regular(2 * nobs + 1)
-  Frf = np.fft.fft(x0, n=n)
-  acf = np.fft.ifft(Frf * np.conjugate(Frf))[:nobs] / nobs
-  acf /= acf[0]
-  acf = np.real(acf[:nlags + 1]) 
-  return acf
+    """
+    Compute autocorrelation using FFT
+    """
+    nobs = len(x)
+    x = np.squeeze(np.asarray(x))
+    x0 = x - x.mean()
+    n = _next_regular(2 * nobs + 1)
+    Frf = np.fft.fft(x0, n=n)
+    acf = np.fft.ifft(Frf * np.conjugate(Frf))[:nobs] / nobs
+    acf /= acf[0]
+    acf = np.real(acf[:nlags + 1])
+    return acf
+
 
 def calc_error(data):
-  """
-  Error estimation for time series of simulation observables and take into
-  account that these series are to some kind degree correlated (which enhances 
-  the estimated statistical error).
-  """
-  data = np.loadtxt(data)
-  # calculate the normalized autocorrelation function of data
-  acf = autocorrelation(data, nlags=1000)
-  # calculate the integrated correlation time tau_int 
-  # (Janke, Wolfhard. "Statistical analysis of simulations: Data correlations 
-  # and error estimation." Quantum Simulations of Complex Many-Body Systems: 
-  # From Theory to Algorithms 10 (2002): 423-445.)
-  tau_int = 0.5
-  k_max = len(acf)
-  for k in range(1,k_max):
-    tau_int += acf[k]
-  
-  # mean value of the time series
-  data_mean = np.mean(data)
-  # calculate the so called effective length of the time series N_eff
-  N_eff = len(data)/(2.0*tau_int)
-  # finally the error is var(data)*sqrt(1/N_eff)
-  stat_err = np.var(data)/np.sqrt(N_eff)
-  return data_mean, stat_err
+    """
+    Error estimation for time series of simulation observables and take into
+    account that these series are to some kind degree correlated (which enhances
+    the estimated statistical error).
+    """
+    data = np.loadtxt(data)
+    # calculate the normalized autocorrelation function of data
+    acf = autocorrelation(data, nlags=1000)
+    # calculate the integrated correlation time tau_int
+    # (Janke, Wolfhard. "Statistical analysis of simulations: Data correlations
+    # and error estimation." Quantum Simulations of Complex Many-Body Systems:
+    # From Theory to Algorithms 10 (2002): 423-445.)
+    tau_int = 0.5
+    k_max = len(acf)
+    for k in range(1, k_max):
+        tau_int += acf[k]
+    # mean value of the time series
+    data_mean = np.mean(data)
+    # calculate the so called effective length of the time series N_eff
+    N_eff = len(data) / (2.0 * tau_int)
+    # finally the error is var(data)*sqrt(1/N_eff)
+    stat_err = np.var(data) / np.sqrt(N_eff)
+    return data_mean, stat_err
