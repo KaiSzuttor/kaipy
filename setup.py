2c08712b0fd61c3253bcb676972866bfe1890695
Kai Szuttor <kai@icp.uni-stuttgart.de>
2015-07-08 18:12:04 +0200

    Removed unneeded lines in setup.py.

--- setup.py
+++ setup.py
@@ -1,7 +1,5 @@
 # Always prefer setuptools over distutils
 from setuptools import setup, find_packages
-# To use a consistent encoding
-from codecs import open
 # Get current version
 execfile('./kaipy/version.py')
 
@@ -13,7 +11,6 @@ setup(
     author='Kai Szuttor',
     author_email='kaiszuttor@gmail.com',
     license='GNU General Public License',
-
     # See https://pypi.python.org/pypi?%3Aaction=list_classifiers
     classifiers=[
         'Development Status :: 3 - Alpha',
@@ -22,17 +19,7 @@ setup(
         'Programming Language :: Python :: 2.7',
         'Topic :: Scientific/Engineering :: Physics'
     ],
-
-    # What does your project relate to?
     keywords='sample setuptools development',
-
-    # You can just specify the packages manually here if your project is
-    # simple. Or you can use find_packages().
     packages=find_packages(exclude=['docs', 'tests*']),
-
-    # List run-time dependencies here.  These will be installed by pip when
-    # your project is installed. For an analysis of "install_requires" vs pip's
-    # requirements files see:
-    # https://packaging.python.org/en/latest/requirements.html
     install_requires=['numpy']
 )
