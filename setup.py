afb74c53976d0e6c31a31744b45bbcc737763429
Kai Szuttor <kai@icp.uni-stuttgart.de>
2015-07-08 18:04:27 +0200

    Added versioning and setup.py.

---
+++ setup.py
@@ -0,0 +1,39 @@
+# Always prefer setuptools over distutils
+from setuptools import setup, find_packages
+# To use a consistent encoding
+from codecs import open
+# Get current version
+execfile('./kaipy/version.py')
+
+setup(
+    name='kaipy',
+    version=__version__,
+    description='A sample Python project',
+    url='https://github.com/KaiSzuttor/kaipy',
+    author='Kai Szuttor',
+    author_email='kaiszuttor@gmail.com',
+    license='GNU General Public License',
+
+    # See https://pypi.python.org/pypi?%3Aaction=list_classifiers
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Science/Research',
+        'License :: OSI Approved :: GNU General Public License v3 or later (GPLv3+)'.
+        'Programming Language :: Python :: 2.7',
+        'Topic :: Scientific/Engineering :: Physics'
+    ],
+
+    # What does your project relate to?
+    keywords='sample setuptools development',
+
+    # You can just specify the packages manually here if your project is
+    # simple. Or you can use find_packages().
+    packages=find_packages(exclude=['docs', 'tests*']),
+
+    # List run-time dependencies here.  These will be installed by pip when
+    # your project is installed. For an analysis of "install_requires" vs pip's
+    # requirements files see:
+    # https://packaging.python.org/en/latest/requirements.html
+    install_requires=['numpy'],
+    },
+)
