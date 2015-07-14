8774381ab2b5d5085fde9cd31c5c4ffe9845ae9d
Kai Szuttor <kai@icp.uni-stuttgart.de>
2015-07-14 16:38:04 +0200

    Added tetscase for rg2.

--- test/test_observable.py
+++ test/test_observable.py
@@ -3,7 +3,7 @@
 
 import unittest
 import numpy as np
-from kaipy.observable import second_legendre
+from kaipy.observable import second_legendre, rg2
 
 class Second_legendre_test(unittest.TestCase):
     def test_x(self):
@@ -33,6 +33,16 @@ class Second_legendre_test(unittest.TestCase):
                                          np.array([1,0,0]),
                                          'z'),
                          -.5)
+
+        
+class Rg2_test(unittest.TestCase):
+    def setUp(self):
+        self.number_of_coordinates = 10000
+        self.coordinates = np.zeros((self.number_of_coordinates,3))
+        self.coordinates[:,0] = np.linspace(0,12,self.number_of_coordinates)
+
+    def test_function(self):
+        self.assertAlmostEqual(rg2(self.coordinates),12,delta=0.01)
 
 if __name__ == "__main__": 
     unittest.main()
