7d4adf7c1552eeb289f22c8f61d0e33f4cafc75f
Kai Szuttor <kai@icp.uni-stuttgart.de>
2016-11-24 20:22:39 +0100

    travis: debug...

--- maintainer/travis/build_docker.sh
+++ maintainer/travis/build_docker.sh
@@ -1,3 +1,4 @@
 #!/usr/bin/env bash
 
-docker run -u kaipy -v ${PWD}:/travis -it kaiszuttor/kaipy:latest /bin/bash -c "pip install codecov --user && git clone /travis && cd travis && pip install . --user && /home/kaipy/.local/bin/coverage run setup.py test && /home/kaipy/.local/bin/codecov"
+ci_env=`bash <(curl -s https://codecov.io/env)`
+docker run $ci_env -u kaipy -v ${PWD}:/travis -it kaiszuttor/kaipy:latest /bin/bash -c "pip install codecov --user && git clone /travis && cd travis && pip install . --user && /home/kaipy/.local/bin/coverage run setup.py test && bash <(curl -s https://codecov.io/bash)"
