1901358c348752f67283dab8ae19ecb70d8da82e
Kai Szuttor <kai@icp.uni-stuttgart.de>
2016-11-24 19:03:35 +0100

    travis: use kaipy user for docker.

--- maintainer/travis/build_docker.sh
+++ maintainer/travis/build_docker.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-docker run -v ${PWD}/travis -it kaiszuttor/kaipy:latest /bin/bash -c "pip install codecov --user && cd travis && pip install . --user && coverage run setup.py test && codecov"
+docker run -u kaipy -v ${PWD}/travis -it kaiszuttor/kaipy:latest /bin/bash -c "pip install codecov --user && cd travis && pip install . --user && coverage run setup.py test && codecov"
