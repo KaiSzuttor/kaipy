9a421158083bf3ff387c8426e5ffb5b9df1ef6ac
Kai Szuttor <kai@icp.uni-stuttgart.de>
2016-11-24 11:06:35 +0100

    Syntax error in utils module.

--- kaipy/utils.py
+++ kaipy/utils.py
@@ -13,9 +13,9 @@ def h5md_pos(h5_dh, ts, folded=True):
     h5_pos = h5_dh["particles/atoms/position/value"][ts,:,:]
     h5_id = h5_dh["particles/atoms/id/value"][ts,:,:]
     h5_image = h5_dh["particles/atoms/image/value"][ts,:,:]
-    sorted_pos = np.array([x for (y, x) in sorted(zip(h5_id, h5_pos))]))
+    sorted_pos = np.array([x for (y, x) in sorted(zip(h5_id, h5_pos))])
     if not folded:
         h5_box = h5_dh["particles/atoms/box/edges"]
-        sorted_image = np.array([x for (y, x) in sorted(zip(h5_id, h5_image))]))
+        sorted_image = np.array([x for (y, x) in sorted(zip(h5_id, h5_image))])
         sorted_pos += sorted_image * box 
     return sorted_pos
