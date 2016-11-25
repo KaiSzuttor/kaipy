c74b1bd58026192041564a7e4d2f06b7f1244a08
Kai Szuttor <kai@icp.uni-stuttgart.de>
2016-11-25 10:24:15 +0100

    travis: fix.

--- maintainer/travis/build_docker.sh
+++ maintainer/travis/build_docker.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
 ci_env=`bash <(curl -s https://codecov.io/env)`
-docker run $ci_env -u kaipy -v ${PWD}:/travis -it kaiszuttor/kaipy:latest /bin/bash -c "echo $HOME && export PATH=$PATH:$HOME/.local/bin && pip install codecov --user && git clone /travis && cd travis && pip install . --user && coverage run setup.py test && bash <(curl -s https://codecov.io/bash)"
+docker run $ci_env -u kaipy -v ${PWD}:/travis -it kaiszuttor/kaipy:latest /bin/bash -c "export PATH=$PATH:/home/kaipy/.local/bin && pip install codecov --user && git clone /travis && cd travis && pip install . --user && coverage run setup.py test && bash <(curl -s https://codecov.io/bash)"
